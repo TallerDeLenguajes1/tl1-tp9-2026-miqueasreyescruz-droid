dccf1cf8599e9806a362d70d85b84be73f17b8bd
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-06-25 19:52:31 -0300

    Modifico el archivo .csproj para no trabajar con variables nullable, tambien creo la estructura basica en el Prgram.cs y agrego la funcion verificarRuta

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// ////////////////////
+//        MAIN
+// ////////////////////
+
+string ruta = verificarRuta();
+
+// ////////////////////
+//     FUNCIONES
+// ////////////////////
+
+// Verica que la ruta sea valida (no este vacia y exista), y devuelve la ruta.
+string verificarRuta()
+{
+    string ruta= "";
+    bool esValido = false;
+
+    while(!esValido)
+    {
+        if (ruta == "") 
+        {
+            Console.WriteLine("Error: La ruta no puede ser vacia. Intente de nuevo.");
+            continue;
+        }
+
+        if (Directory.Exists(ruta))
+        {
+            esValido = true;
+        }
+        else
+        {
+            Console.WriteLine("Error: La ruta ingresada no existe. Intente de nuevo");
+        }
+    }
+
+    return ruta;
+}
