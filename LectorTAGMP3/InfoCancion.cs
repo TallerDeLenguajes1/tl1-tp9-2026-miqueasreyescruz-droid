cfa1270b831d0d36eb6e9891225f23acc2a88867
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-06-25 23:22:02 -0300

    Creo la clase Id3v1Tag en el archivo InfoCanciones

--- LectorTAGMP3/InfoCancion.cs
+++ LectorTAGMP3/InfoCancion.cs
@@ -1,1 +1,22 @@
 namespace InfoCanciones;
+
+public class Id3v1Tag
+{
+    private string _titulo;
+    private string _artista;
+    private string _album;
+    private string _fechapublicacion;
+
+    public string Titulo { get => _titulo; }
+    public string Artista { get => _artista; }
+    public string Album { get => _album; }
+    public string Fechapublicacion { get => _fechapublicacion; }
+
+    public Id3v1Tag (string _titulo, string _artista, string _album, string _fechapublicacion)
+    {
+        this._titulo = _titulo;
+        this._artista = _artista;
+        this._album = _album;
+        this._fechapublicacion = _fechapublicacion;
+    }
+}
