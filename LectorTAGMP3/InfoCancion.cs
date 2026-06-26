72d7a04852de5c686ecf05c48731dd1ca6cd4175
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-06-26 01:22:03 -0300

    Arreglo un problema con el nombre de los campos de la clase

--- LectorTAGMP3/InfoCancion.cs
+++ LectorTAGMP3/InfoCancion.cs
@@ -5,18 +5,18 @@ public class Id3v1Tag
     private string _titulo;
     private string _artista;
     private string _album;
-    private string _fechapublicacion;
+    private string _anio;
 
     public string Titulo { get => _titulo; }
     public string Artista { get => _artista; }
     public string Album { get => _album; }
-    public string Fechapublicacion { get => _fechapublicacion; }
+    public string Anio { get => _anio; }
 
-    public Id3v1Tag (string _titulo, string _artista, string _album, string _fechapublicacion)
+    public Id3v1Tag (string _titulo, string _artista, string _album, string _anio)
     {
         this._titulo = _titulo;
         this._artista = _artista;
         this._album = _album;
-        this._fechapublicacion = _fechapublicacion;
+        this._anio = _anio;
     }
 }
