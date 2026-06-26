a0934084c6884196b04b593a8fae8ded58feeb7f
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-06-25 23:47:54 -0300

    Creo la estructura basica del programa y tambien agrego la funcion verificarRuta

--- LectorTAGMP3/Program.cs
+++ LectorTAGMP3/Program.cs
@@ -1,1 +1,59 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// ===========================
+//         ESPACIOS
+// ===========================
+
+using InfoCancion;
+
+// ===========================
+//           MAIN   
+// ===========================
+
+Console.WriteLine("============================");
+Console.WriteLine("  LECTOR DE ARCHIVOS ID3v1  ");
+Console.WriteLine("============================");
+
+string ruta = verificarRuta();
+
+
+
+// ===========================
+//         FUNCIONES
+// ===========================
+
+/// Usamos la misma funcion, pero con una modificacion para tambien verificar la extencion del archivo
+string verificarRuta()
+{
+    string path= "";
+    string extension = "";
+    bool esValido = false;
+
+    while(!esValido)
+    {
+        Console.Write("-> Ingrese la ruta del archivo: ");
+        path = Console.ReadLine(); 
+
+        if (path == "") 
+        {
+            Console.WriteLine("Error: La ruta no puede ser vacia. Intente de nuevo.\n");
+            continue;
+        }
+
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Error: La ruta ingresada no existe. Intente de nuevo.\n");
+            continue;
+        }
+
+        extension = Path.GetExtension(path);
+        if (extencion.ToLower() == ".mp3")
+        {
+            esValido = true;
+        }
+        else
+        {
+            Console.WriteLine("Error: El archivo debe ser formato \".mp3\" . Intente de nuevo.\n");
+        }
+    }
+
+    return path;
+}
